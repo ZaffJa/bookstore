3d5d464196bb409552be7f0b1e5ac080cc86cc96
Zafri Zulkipli <zafrizulkipli@gmail.com>
2017-05-25 20:03:02 +0800

    Change icon

--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -3,8 +3,8 @@
         <div class="container">
             <ul class="mainnav">
                 <li {{ setActive('/') }}><a href="/"><i class="icon-dashboard"></i><span>Dashboard</span> </a> </li>
-                <li {{ setActive('book') }}><a href="/book"><i class="icon-money"></i><span>Books</span> </a> </li>
-                <li {{ setActive('transaction') }}><a href="/transaction"><i class="icon-forward"></i><span>Transaction</span> </a></li>
+                <li {{ setActive('book') }}><a href="/book"><i class="icon-book"></i><span>Books</span> </a> </li>
+                <li {{ setActive('transaction') }}><a href="/transaction"><i class="icon-money"></i><span>Transaction</span> </a></li>
             </ul>
         </div>
     </div>
